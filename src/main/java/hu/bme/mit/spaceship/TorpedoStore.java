36734580d9d681ad84e8ee043b178bf1f0e10d42
unknown <184346@BME.HU>
2024-10-09 09:23:18 +0200

    PallaiChanges

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -39,6 +39,7 @@ public class TorpedoStore {
     // simulate random overheating of the launcher bay which prevents firing
 
     double r = this.generator.nextDouble();
+    //paradicsom
 
     if (r >= FAILURE_RATE) {
       // successful firing
